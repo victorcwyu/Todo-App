c79bc631b4e9fc7db37ecdf72ff1c90338d4cdaf
Victor <victorcwyu@gmail.com>
2020-09-10 02:15:25 -0400

    Removed some comments and added Font Awesome icon.

--- src/components/ChecklistItem.js
+++ src/components/ChecklistItem.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import "../styles/ChecklistItem.scss";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
 function ChecklistItem() {
   const [checked, setChecked] = useState("");
 
@@ -12,8 +15,9 @@ function ChecklistItem() {
 
   return (
     <div className="checklistItemContainer">
-      <input type="checkbox" checked={checked} onChange={handleChange}></input>
+      <input type="checkbox" checked={checked} onChange={handleChange} />
       {checked === "" ? <h2>To do!</h2> : <h2>Checked off the list!</h2>}
+      <FontAwesomeIcon icon={faTrash} />
     </div>
   );
 }
