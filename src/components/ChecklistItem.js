6188ccc43e8ea6fa8e9acba2389777f7f557e5ec
Victor <victorcwyu@gmail.com>
2020-09-10 01:53:49 -0400

    Added functionality to ChecklistItem.

--- src/components/ChecklistItem.js
+++ src/components/ChecklistItem.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/ChecklistItem.scss";
 
 function ChecklistItem() {
+  const [checked, setChecked] = useState("");
+
+  const handleChange = () => {
+    if (checked === "") {
+      setChecked("checked");
+    } else setChecked("");
+  };
+
   return (
     <div className="checklistItemContainer">
-      <h2>Checklist Item</h2>
+      <input type="checkbox" checked={checked} onChange={handleChange}></input>
+      {checked === "" ? <h2>To do!</h2> : <h2>Checked off the list!</h2>}
     </div>
   );
 }
